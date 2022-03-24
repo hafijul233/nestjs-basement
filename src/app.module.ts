7d154d294e8e752313bd4be5c0b948307d81ff8c
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-03-24 14:24:18 +0600

    nest mandatory packages added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/config.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [
+    AppConfigModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppConfigService } from './config/app/config.service';
+import * as compression from 'compression';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
   const appConfigService: AppConfigService = app.get(AppConfigService);
   app.setGlobalPrefix('api');
+  app.use(helmet());
+  app.use(compression()); //disabled if server compression is enabled
 
   await app.listen(appConfigService.port);
 }
