74fed09427686d9cf792bde63eacc5566a524ba8
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-03-24 14:35:04 +0600

    open API to dev env updated

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,37 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppConfigService } from './config/app/config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as compression from 'compression';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
   const appConfigService: AppConfigService = app.get(AppConfigService);
+
   app.setGlobalPrefix('api');
+
   app.use(helmet());
+
   app.use(compression()); //disabled if server compression is enabled
+
+  //openapi boot code
+  if (appConfigService.env === 'local') {
+    const config = new DocumentBuilder()
+      .setTitle('My Cash Money API Documentation')
+      .setDescription(
+        'My Cash Money Remittance transfer service provider and Web Api System.',
+      ).setVersion('1.0.0')
+      .setContact('My Cash Money', 'https://mycash.my/', 'support@mycashmy.com')
+      .setTermsOfService('https://' + appConfigService.url + ':' +
+        appConfigService.port + '/terms-condition')
+      .setLicense('My Cash Money', 'My Cash Money License')
+      .build();
+    const document = SwaggerModule.createDocument(app, config, {
+      ignoreGlobalPrefix: true,
+    });
+    SwaggerModule.setup('docs', app, document);
+  }
 
   await app.listen(appConfigService.port);
 }
